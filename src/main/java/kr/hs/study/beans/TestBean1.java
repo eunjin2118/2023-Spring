fe461a384d0f776efa7598094b87881a6bdfaef9
eunjin2118 <ally6324@naver.com>
2023-03-09 16:07:26 +0900

    first commit

---
+++ src/main/java/kr/hs/study/beans/TestBean1.java
@@ -0,0 +1,7 @@
+package kr.hs.study.beans;
+
+public class TestBean1 implements TestBean{
+    public void test(){
+        System.out.println("hello");
+    }
+}
---
+++ src/main/java/kr/hs/study/beans/TestBean2.java
@@ -0,0 +1,7 @@
+package kr.hs.study.beans;
+
+public class TestBean2 implements TestBean{
+    public void test() {
+        System.out.println("hello");
+    }
+}
