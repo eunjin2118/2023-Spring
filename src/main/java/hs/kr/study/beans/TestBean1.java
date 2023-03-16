a379de0ab09439c5a78fac39f207da5c5585386c
Eunjin Pyeon <s2131@e-mirim.hs.kr>
2023-03-16 14:44:43 +0900

    init-method, destroy_method

---
+++ src/main/java/hs/kr/study/beans/TestBean1.java
@@ -0,0 +1,15 @@
+package hs.kr.study.beans;
+
+public class TestBean1 {
+    TestBean1() {
+
+    }
+
+    public void init_method(){
+        System.out.println("TestBean1의 init method");
+    }
+    public void destroy_method(){
+        System.out.println("TestBean1의 destroy method");
+    }
+
+}
