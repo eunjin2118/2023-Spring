3cf9e72e0473854d2a02b8b2637720d44fc16748
eunjin2118 <s2131@e-mirim.hs.kr>
2023-03-18 12:49:41 +0900

    get member

--- src/main/java/study/db/MemberController.java
+++ src/main/java/study/db/MemberController.java
@@ -1,23 +1,28 @@
 package study.db;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import study.db.entity.Member;
 
 import java.util.HashMap;
 import java.util.Map;
+import java.util.Optional;
 
 @RestController
 public class MemberController {
     @Autowired MemberRepository repository;
 
-    @PostMapping("./add_memeber")
+    @PostMapping("/add_member")
     public Map<String, Object> addMember(@RequestBody Member member) {
         repository.save(member);
         Map<String, Object> result = new HashMap<>();
         result.put("result", "success");
         return result;
+    }
+
+    @GetMapping("/get_member/{id}")
+    public Member getMember(@PathVariable("id") Integer id) {
+        Optional<Member> result = repository.findById(id);
+        return result.get();
     }
 }
