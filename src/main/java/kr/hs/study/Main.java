1e6e1f1408aa6345f790afa6f9838e0a8cbe8823
Eunjin Pyeon <s2131@e-mirim.hs.kr>
2023-04-06 15:38:44 +0900

    Qualifier를 이용해서 객체에 이름 주기

--- src/main/java/kr/hs/study/Main.java
+++ src/main/java/kr/hs/study/Main.java
@@ -1,6 +1,7 @@
 package kr.hs.study;
 
 import kr.hs.study.beans.TestBean1;
+import kr.hs.study.beans.TestBean2;
 import kr.hs.study.config.BeanConfig;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
@@ -13,6 +14,11 @@ public class Main {
         System.out.println("t1 : " + t1);
         System.out.println("data1 : " + t1.getData1());
         System.out.println("data2 : " + t1.getData2());
+
+        TestBean2 t2 = ctx.getBean(TestBean2.class);
+        System.out.println("data1 : " + t2.getData1());
+        System.out.println("data2 : " + t2.getData2());
+
         ctx.close();
     }
 }
--- src/main/java/kr/hs/study/config/BeanConfig.java
+++ src/main/java/kr/hs/study/config/BeanConfig.java
@@ -1,10 +1,23 @@
 package kr.hs.study.config;
 
+import kr.hs.study.beans.DataBean2;
+import kr.hs.study.beans.TestBean2;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 @ComponentScan(basePackages = "kr.hs.study.beans")
 public class BeanConfig {
+
+    @Bean
+    public DataBean2 java1(){
+        return new DataBean2();
+    }
+
+    @Bean
+    public DataBean2 java2(){
+        return new DataBean2();
+    }
 
 }
